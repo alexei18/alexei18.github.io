9644d95d38b7087f119072a3ac4baf1c1a6f02be
alexei18 <aonea1828@gmail.com>
2025-02-10 18:48:34 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let leftPos = 0
+let bottomPos = 0
+document.getElementById('yessir').addEventListener('click', () => {
+    alert('Știam că mă iubești! 😍. Să știi că eu tot te iubesc ❤️, dacă nu ai încercat să pui NU, încearcă, când vei reușii atunci nu ne vom iubii.😘😘')
+})
+document.getElementById('nooo').addEventListener('mouseover', () => {
+    leftPos = Math.floor(Math.random() * (50 - (-50) + 1)) + (-50);
+    bottomPos = Math.floor(Math.random() * (100 - (-100) + 1)) + (-100);
+    document.getElementById('nooo').style.position = "absolute"
+    document.getElementById('nooo').style.left = leftPos + "%"
+    document.getElementById('nooo').style.bottom = bottomPos + "%"
+})
+document.getElementById('nooo').addEventListener('click', () => {
+    leftPos = Math.floor(Math.random() * (50 - (-50) + 1)) + (-50);
+    bottomPos = Math.floor(Math.random() * (100 - (-100) + 1)) + (-100);
+    document.getElementById('nooo').style.position = "absolute"
+    document.getElementById('nooo').style.left = leftPos + "%"
+    document.getElementById('nooo').style.bottom = bottomPos + "%"
+})
